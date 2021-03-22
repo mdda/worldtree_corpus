b8a694040268c6bb699be9bc7f31221a5674c17b
Martin Andrews <GitHub@mdda.net>
2021-03-23 00:52:19 +0800

    Minor updates

--- run_setup.bash
+++ run_setup.bash
@@ -4,7 +4,7 @@ set -e
 # Get a copy of the desk reference here...
 PDF=worldtree_explanation_corpus_v2.1_book_desk_reference_draft.pdf
 if [ ! -f "$PDF" ]; then
-    wget http://cognitiveai.org/dist/${PDF}
+    #wget http://cognitiveai.org/dist/${PDF}  # Optional, TBH
 fi
 
 TASK_REPO=tg2021task
@@ -18,7 +18,7 @@ VENV=env38
 if [ ! -d "$VENV" ]; then
     virtualenv --system-site-packages ${VENV}
 fi
-. env3/bin/activate
+. env38/bin/activate
 
 pip3 install -r requirements.txt
 pip3 install -r ${TASK_REPO}/requirements.txt   # Already included in this repo
